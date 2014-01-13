8aa4d012e4029d121ae4d70739ec7dac398ef96e
Praveen Shangunathan <praveens@yahoo-inc.com>
2014-01-12 23:23:44 -0800

    first commit

---
+++ src/com/project/androidtipcalculator/TipCalculateActivity.java
@@ -0,0 +1,22 @@
+package com.project.androidtipcalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TipCalculateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tip_calculate);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tip_calculate, menu);
+		return true;
+	}
+
+}
